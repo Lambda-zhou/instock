cac1a6a4056529f119cc106a5e41368ab513f7f9
Lambda-zhou <68288225+Lambda-zhou@users.noreply.github.com>
2024-12-28 16:21:09 +0800

    Update strategy_data_daily_job.py

--- job/strategy_data_daily_job.py
+++ job/strategy_data_daily_job.py
@@ -13,7 +13,6 @@ cpath_current = os.path.dirname(os.path.dirname(__file__))
 cpath = os.path.abspath(os.path.join(cpath_current, os.pardir))
 sys.path.append(cpath)
 import instock.lib.run_template as runt
-import instock.core.tablestructure as tbs
 from instock.core.singleton_stock import stock_hist_data
 from instock.core.stockfetch import fetch_stock_top_entity_data
 
