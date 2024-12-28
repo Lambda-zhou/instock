4c8c0f1f171bebdf2dc6e67822027b7672249002
Lambda-zhou <68288225+Lambda-zhou@users.noreply.github.com>
2024-12-28 16:20:38 +0800

    Update indicators_data_daily_job.py

--- job/indicators_data_daily_job.py
+++ job/indicators_data_daily_job.py
@@ -14,7 +14,6 @@ cpath_current = os.path.dirname(os.path.dirname(__file__))
 cpath = os.path.abspath(os.path.join(cpath_current, os.pardir))
 sys.path.append(cpath)
 import instock.lib.run_template as runt
-import instock.core.tablestructure as tbs
 import instock.core.indicator.calculate_indicator as idr
 from instock.core.singleton_stock import stock_hist_data
 
