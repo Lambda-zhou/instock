e9deb7a54deec88303472048551e9bc849580c7f
Lambda-zhou <68288225+Lambda-zhou@users.noreply.github.com>
2024-12-28 16:20:05 +0800

    Update backtest_data_daily_job.py

--- job/backtest_data_daily_job.py
+++ job/backtest_data_daily_job.py
@@ -14,7 +14,6 @@ import push
 cpath_current = os.path.dirname(os.path.dirname(__file__))
 cpath = os.path.abspath(os.path.join(cpath_current, os.pardir))
 sys.path.append(cpath)
-import instock.core.tablestructure as tbs
 import instock.core.backtest.rate_stats as rate
 from instock.core.singleton_stock import stock_hist_data
 
